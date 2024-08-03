be58ffb9f5974fa39544ec65e910a34916d57e31
Daniel Pintado Várez <dapinva95@gmail.com>
2024-08-03 14:14:45 +0200

    feat: Agregar rutas de tareas y autenticación

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -5,18 +5,39 @@ import { Router } from 'express';
 import { authRequired } from '../middlewares/validateToken.js';
 
 //Importamos las rutas
-
+import { 
+    obtenerTaks, 
+    obtenerTaskID, 
+    crearTask, 
+    modificarTask, 
+    eliminarTask } from '../controllers/tasks.controller.js';
 
 //Inicializamos router
 const router = Router();
 
-//Creamos una ruta llamada tasks que solo se puede acceder si el usuario está autenticado
-router.get('/tasks', authRequired, (req, res) =>{
-    
-    console.log('Tasks');
-    res.send('Tasks');
-    
-});
+
+
+//CRUD de tareas
+
+
+//Definimos las rutas
+//A estas riuas solo se puede acceder si se está autenticado
+
+
+//Obtener todas las tareas
+router.get('/tasks', authRequired, obtenerTaks,(req, res) =>{});
+
+//Obtener una tarea por id
+router.get('/tasks/:id', authRequired, obtenerTaskID, (req, res) =>{});
+
+//Crear una tarea
+router.post('/tasks', authRequired, crearTask, (req, res) =>{});
+
+//Actualizar/Modificar una tarea
+router.put('/tasks/:id', authRequired, modificarTask, (req, res) =>{});
+
+//Eliminar una tarea
+router.delete('/tasks/:id', authRequired, eliminarTask, (req, res) =>{});
 
 //Exportamos router
 export default router;
