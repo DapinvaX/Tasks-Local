fced3305ee204bd251af4416f1b4dc66e2695cde
DapinvaX <dapinva95@gmail.com>
2024-09-05 21:46:32 +0200

    feat: Modificación de axios.js (API).
    Creada constante llamada "instancia" que contendrá la URL base, los headers y withCredentials en "true" que permitirá enviar las credenciales a través de las cookies junto con las solicitudes.

--- Tasks-Frontend/src/API/axios.js
+++ Tasks-Frontend/src/API/axios.js
@@ -4,9 +4,14 @@ import axios from 'axios';
 //Declaramos la constante API que almacena la URL de la API
 const API = 'http://localhost:4000/api';
 
-axios.create({
+const instancia = axios.create({
     baseURL: API,
     headers: {
         'Content-Type': 'application/json',
     },
+
+    //Permite enviar credenciales a través de las cookies en las solicitudes
+    withCredentials: true
 });
+
+export default instancia;
