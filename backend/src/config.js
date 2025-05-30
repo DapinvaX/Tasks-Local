bfd215199b4bccdeb6edf6f21a7947f0b7a8486e
DapinvaX <dapinva95@gmail.com>
2025-05-30 15:25:41 +0200

    feat: Cargar variables de entorno desde .env y actualizar TOKEN_SECRET para mejorar la seguridad

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,2 +1,7 @@
+import dotenv from 'dotenv';
+
+// Cargamos las variables de entorno desde el archivo .env
+dotenv.config();
+
 //Guardamos el TOKEN_SECRET en una variable para poder exportarlo y usarlo en otros archivos
-export const TOKEN_SECRET = 'MySecretKeyDPX';
+export const TOKEN_SECRET = process.env.TOKEN_SECRET;
